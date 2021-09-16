d384052ca0ef81929b1568af97513287a340f7e5
AbrarNoob <hasinabrarhaque@gmail.com>
2021-09-16 14:56:30 +0600

    BananaDroid is now OFFICIAL

--- banana_lavender.mk
+++ banana_lavender.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common BananaDroid stuff
 $(call inherit-product, vendor/banana/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-
+BANANA_BUILDTYPE := OFFICIAL
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
