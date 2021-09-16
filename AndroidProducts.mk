9bb52534b23729d09b255b4477b0dae5432306c8
AbrarNoob <hasinabrarhaque@gmail.com>
2021-09-16 14:02:07 +0600

    lavender:BananaDroid Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/banana_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lavender-user \
-    aosp_lavender-userdebug \
-    aosp_lavender-eng
+    banana_lavender-user \
+    banana_lavender-userdebug \
+    banana_lavender-eng
